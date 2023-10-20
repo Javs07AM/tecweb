3c81010f4d96595c750cbdb583d82c7c90674f04
JAVIER <aguilarmaciasjavier11@gmail.com>
2023-10-20 13:54:33 -0600

    Modificando imagen default

--- practicas/p09/product_app/app.js
+++ practicas/p09/product_app/app.js
@@ -5,7 +5,7 @@ var baseJSON = {
     "modelo": "XX-000",
     "marca": "NA",
     "detalles": "NA",
-    "imagen": "img/default.png"
+    "imagen": "http://localhost/tecwebCarlosactividades/01-la_web_estatica/img/img.png"
   };
 
 function init() {
